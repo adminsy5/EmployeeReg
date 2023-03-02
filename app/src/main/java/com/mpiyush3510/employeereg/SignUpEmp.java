08da7dc17e27d16c5c58e02fd11d289662230959
PIYUSH MAKWANA <mpiyush3510@cbigujarat.onmicrosoft.com>
2023-03-03 00:41:10 +0530

    Ui Update 1.2

--- app/src/main/java/com/mpiyush3510/employeereg/SignUpEmp.java
+++ app/src/main/java/com/mpiyush3510/employeereg/SignUpEmp.java
@@ -1,13 +1,13 @@
 package com.mpiyush3510.employeereg;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.mpiyush3510.employeereg.databinding.ActivitySignUpEmpBinding;
@@ -20,6 +20,7 @@ String[] frnds={"Rdx","Mpiyu","Ak"};
         super.onCreate(savedInstanceState);
         binding = ActivitySignUpEmpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
         AdminListeners();
         binding.BtnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,8 +32,16 @@ String[] frnds={"Rdx","Mpiyu","Ak"};
         ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_spinner_item,frnds);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         binding.spin1.setAdapter(arrayAdapter);
-    }
 
+        binding.hyperLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url="https://sutexianinc.w3spaces.com/index.html";
+                Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+    }
     private void AdminListeners(){
         DatePicker();
     }
