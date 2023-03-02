857c20c341f642fe69a8745d2f25dc49220d75b2
PIYUSH MAKWANA <mpiyush3510@cbigujarat.onmicrosoft.com>
2023-03-03 02:43:22 +0530

    Validation Update 1.4

--- app/src/main/java/com/mpiyush3510/employeereg/SignUpEmp.java
+++ app/src/main/java/com/mpiyush3510/employeereg/SignUpEmp.java
@@ -1,13 +1,19 @@
 package com.mpiyush3510.employeereg;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.mpiyush3510.employeereg.databinding.ActivitySignUpEmpBinding;
 
 public class SignUpEmp extends AppCompatActivity {
@@ -30,8 +36,9 @@ String[] Designation={"Developer","Engineer"};
         binding.BtnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
+                if(checkValidation()){
+                    //Toast.makeText(SignUpEmp.this, "Very Good", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         // ArrayAdapter<String> arrayAdapter1=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,Designation);
@@ -60,5 +67,66 @@ String[] Designation={"Developer","Engineer"};
                 });
             }
         });
+    }
+
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+    }
+    private Boolean checkValidation(){
+        if(binding.EmpEditName.getText().toString().isEmpty()){
+            showToast("Please Enter Employee Name !");
+            return false;
+        }else if(binding.EmpEditEmail.getText().toString().isEmpty()){
+            showToast("Email Can't be empty !");
+            return false;
+        }else if (!Patterns.EMAIL_ADDRESS.matcher(binding.EmpEditEmail.getText().toString()).matches()){
+            showToast("Please enter Valid Email !");
+            return false;
+        } else if (!binding.empEditmale.isChecked() && !binding.empEditFemale.isChecked()) {
+            showToast("Please Choose Gender !");
+            return false;
+        } else if (binding.EmpJoinDate.getText().toString().isEmpty()) {
+            showToast("Please Select Jdate !");
+            return false;
+        } else if (binding.EmpEditMno.getText().toString().isEmpty()) {
+            showToast("Please Add Your MobileNo !");
+            return false;
+        } else if (!Patterns.PHONE.matcher(binding.EmpEditMno.getText().toString()).matches()) {
+            showToast("Please enter Correct PhoneNo !");
+            return false;
+        } else if (!binding.empEditBcom.isChecked() && !binding.empEditBca.isChecked() && !binding.empEditBba.isChecked()) {
+            showToast("Choose Your Course !");
+            return false;
+        } else if (binding.EmpEditPasswd.getText().toString().isEmpty()) {
+            showToast("Please Enter Password");
+            return false;
+        } else if (binding.EmpEditPasswd.getText().toString().matches(binding.EmpEditEmail.getText().toString()) ||
+                binding.EmpEditPasswd.getText().toString().matches(binding.EmpEditMno.getText().toString()) ||
+                binding.EmpEditPasswd.getText().toString().matches(binding.EmpEditName.getText().toString()) ) {
+            showToast(" Try Again !");
+            custAlert();
+            return false;
+        }else if (!binding.EmpCheckTerms.isChecked()){
+            showToast("Please accept terms & Condition !");
+            return false;
+    }else{
+            showToast("Good");
+        }
+        return true;
+    }
+
+    private void custAlert(){
+        MaterialAlertDialogBuilder builder=new MaterialAlertDialogBuilder(this)
+                .setTitle("Error")
+                .setMessage(R.string.dialogue_mess)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        showToast("Developer : Piyush Makwana ⭐");
+                        dialog.dismiss();
+                    }
+                });
+        builder.create();
+        builder.show();
     }
 }
